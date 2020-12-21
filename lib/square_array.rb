906be3fed0a5ea7bc994b3f8fb76da1363578ffe
Michael Rabe <m.trabe96@gmail.com>
2020-12-21 23:42:26 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
-array=[1, 2, 3]
+numbers=[1, 2, 3]
 square_array=[]
 
-array.length.times do 
-  square_array.push(array) ** 2)
+numbers.length.times do 
+  square_array.push(numbers) ** 2)
 end
 square_array
