9f3bd16bbc615a61b843cb829da902740da99f58
Michael Rabe <m.trabe96@gmail.com>
2020-12-22 00:04:47 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ numbers=[1, 2, 3]
 
 def square_array (numbers) 
   square_array = []
-  numbers.length.times do |numbers|
+  while numbers.length.times do |numbers|
   square_array << numbers ** 2
   end
   square_array
